2fca5c23bff06a1f60b8eec1e9e43ac509711460
TheWebTuber <atomkhanthak@gmail.com>
2025-02-05 16:16:08 +0100

    terurn cookie

--- js/cookies.js
+++ js/cookies.js
@@ -36,19 +36,3 @@
 //     console.log("All cookies disabled");
 //   }
 // });
-
-
-
-
-// <!-- Cookie Consent by TermsFeed https://www.TermsFeed.com -->
-// <script type="text/javascript" src="https://www.termsfeed.com/public/cookie-consent/4.2.0/cookie-consent.js" charset="UTF-8"></script>
-// <script type="text/javascript" charset="UTF-8">
-// document.addEventListener('DOMContentLoaded', function () {
-// cookieconsent.run({"notice_banner_type":"headline","consent_type":"express","palette":"dark","language":"en","page_load_consent_levels":["strictly-necessary"],"notice_banner_reject_button_hide":false,"preferences_center_close_button_hide":false,"page_refresh_confirmation_buttons":false,"website_name":"Duffball16","website_privacy_policy_url":"https://duffball16.com/privacypolicy"});
-// });
-// </script>
-
-// <noscript>Free cookie consent management tool by <a href="https://www.termsfeed.com/">TermsFeed</a></noscript>
-// <!-- End Cookie Consent by TermsFeed https://www.TermsFeed.com -->
-
-// <a href="#" id="open_preferences_center">Update cookies preferences</a>
