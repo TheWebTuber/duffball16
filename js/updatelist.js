4069fc24b6a8985a08b55b61d85fd9a4b4157fde
TheWebTuber <atomkhanthak@gmail.com>
2025-02-07 18:34:40 +0100

    good update list

--- js/updatelist.js
+++ js/updatelist.js
@@ -3,14 +3,14 @@ const posts = [
         id: "post1",
         title: "New Feature: Improved Search!",
         date: "Published: October 26, 2023",
-        image: "images/search-feature.jpg",
+        image: "img/duffball16banner.jpg",
         text: "We're excited to announce a significant improvement to our search functionality! You can now find what you're looking for faster and more accurately. We've implemented a new algorithm that takes into account user behavior, semantic analysis, and real-time indexing. This means your searches will be more relevant and timely. We've also added auto-suggestions and improved filtering options. Try it out today and let us know what you think!"
     },
     {
         id: "post2",
         title: "Website Redesign: A Fresh New Look",
         date: "Published: October 15, 2023",
-        image: "images/redesign-screenshot.jpg",
+        image: "img/pfpduffball16.png",
         text: "As you may have noticed, we've given the website a complete makeover! The goal was to create a more user-friendly and visually appealing experience. We've updated the color scheme, typography, and layout to be more modern and accessible. We hope you enjoy the new design!"
     }
 ];
