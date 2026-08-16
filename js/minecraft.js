0c5a706bfdf83a3384ad26ac9513eb18ddf72562
TheWebTuber <atomkhanthak@gmail.com>
2026-08-16 22:11:18 +0200

    test new pages with year copyright?

--- js/minecraft.js
+++ js/minecraft.js
@@ -63,6 +63,3 @@ async function fetchStatus() {
 fetchStatus();
 setInterval(fetchStatus, 60000);
 
-document.querySelectorAll("[data-year]").forEach((year) => {
-  year.textContent = new Date().getFullYear();
-});
